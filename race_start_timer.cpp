53b8aa03bd62bddb69ced14f1b624ee74a069bcf
cameron_acer <cameron97@talktalk.net>
2022-03-03 22:57:40 +0000

    Added a micro python prototype.
    Code starts immediately, counts down from -5:30, printing the time to the console every second. The LED flashed and "toot" is printed to the console when a sound signal is made.
    
    Comments laying out the future work needed in the main file

---
+++ race_start_timer.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+str time = 5; 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int
+main()
+{
+    
+printf("time", "\n");
+    
+}
