fdaeb677ec3a3618fe6dea488905c7933e9b42e7
cameron_acer <cameron97@talktalk.net>
2022-03-09 23:59:26 +0000

    The display is now run in a separate thread.
    When you start it you get this error:
    "NB! Thonny could not execute 'Run'.
    It looks like MicroPython has arrived to an unexpected state or there have been communication problems. See Thonny's backend.log for more information.
    You may need to reconnect, Stop/Restart or hard-reset your device.
    
    ManagementError
    >>>
    NB! Thonny could not execute 'get_globals'.
    It looks like MicroPython has arrived to an unexpected state or there have been communication problems. See Thonny's backend.log for more information.
    You may need to reconnect, Stop/Restart or hard-reset your device."
    
    Sometimes the code freezes and prints something like "004 4 : : 4646" when it should print "04:46"
    
    I think it's because there is no lock to stop both threads accessing the global variable display_time at the same time.

--- race_start_timer.py
+++ race_start_timer.py
@@ -1,11 +1,36 @@
 from machine import Pin, Timer
-#import time
+import _thread
+import time
 
-#some definitions
+#define variables
 elapsed = int(-330) #the elapsed time from the start of the race. It starts at -5minutes and 30seconds
-led = Pin(25, Pin.OUT) #the LED on the board is on pin 25. Needs to be changed to the relay pin
+display_time = str(1531) #the time to write to the display. This must be a 4 character string. This is using a global variable which is a slightly messy way of doing it
 
-#a function to take in the time and display it on the 7 seg display
+#define pins
+led = Pin(25, Pin.OUT) #the LED on the board is on pin 25. Needs to be changed to the relay pin
+relay = Pin(0, Pin.OUT) #the GPIO pin to connect the relay for the hooter to
+#defining the pins used for the display
+display_1 = Pin(1, Pin.OUT)
+display_2 = Pin(2, Pin.OUT)
+display_3 = Pin(3, Pin.OUT)
+display_4 = Pin(4, Pin.OUT)
+display_5 = Pin(5, Pin.OUT)
+display_7 = Pin(6, Pin.OUT)
+display_10 = Pin(7, Pin.OUT)
+display_11 = Pin(8, Pin.OUT)
+
+segments = [display_1 , display_2 , display_3 , display_4 , display_5 , display_7 , display_10 , display_11]
+
+display_12 = Pin(9, Pin.OUT)
+display_9 = Pin(10, Pin.OUT)
+display_8 = Pin(11, Pin.OUT)
+display_6 = Pin(13, Pin.OUT)
+
+digits = [display_12 , display_9 , display_8 , display_6]
+for k in range (0,3):
+    digits[k].value(1) #set all the multiplexer pins to be high. They are pulled low to address the digit
+
+#define functions
 
 def stop(timer):
     led.value(0)
@@ -26,14 +51,53 @@ def toot_short():
     #led.value(0)
     tom.init(mode=Timer.ONE_SHOT, period=500, callback=stop)
     
+def display_write():
+    global display_time
+    global segments
+    global digits
+    truths = [[1,1,0,1,0,1,1,1],[0,0,0,1,0,1,0,0],[1,1,0,0,1,1,0,1],[0,1,0,1,1,1,0,1],[0,0,0,1,1,1,1,0],[0,1,0,1,1,0,1,1],[1,1,0,1,1,0,1,1],[0,0,0,1,0,1,0,1],[1,1,0,1,1,1,1,1],[0,0,0,1,1,1,1,1]]  #the truth table for  all the pin values to give each digit
+    
+    while True:
+        for i in range (0,4):
+            digits[i].value(0)
+            digit = int(display_time[i])
+            #print(display_time)
+            #print(i)
+            #print(digit)
+            for j in range (0,8):
+                segments[j].value((truths[digit][j]))
+            time.sleep(0.0015)
+            for j in range (0,8):
+                segments[j].value(0)
+            digits[i].value(1)
+            
+        #print(display_time)
+
+        
+    
 
 tim = Timer()#the timer for the main clock
 tom = Timer()#the timer for the toot length
 
 def tick(timer):#the periodic timer that increments the elapsed time by 1 second, prints the time, and checks whether a sound signal is needed
     global elapsed
+    global display_time
     elapsed += 1
-    print(abs(elapsed)//60, ':', abs(elapsed)%60) #Prints the absolute elapsed time in minuets and seconds. Needs to be changed to pass this to the display function
+    
+    #micropython has no zfill(). This ensures the minutes ad seconds are not one digits by adding preceding 0s. This programm isn't designed to work after 99 minutes
+    mint = str(abs(elapsed)//60) #mint is minutes because min is already a function
+    if len(mint) == 1:
+        mint = "0" + mint
+        
+    sec = str(abs(elapsed)%60)
+    if len(sec) == 1:
+        sec = "0" + sec
+    
+    display_time = mint + sec
+    
+    print(mint, ':' , sec)
+    #print(display_time)
+      
     if elapsed == -300:
         toot_short()
     elif elapsed == -240:
@@ -44,9 +108,9 @@ def tick(timer):#the periodic timer that increments the elapsed time by 1 second
         toot_short()
     
 #a separate thread on the other core to run the 7 seg display. 
+_thread.start_new_thread(display_write, ())
 
-tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
- #calls the periodic timer with a frequency of 1Hz
+tim.init(freq=1, mode=Timer.PERIODIC, callback=tick) #calls the periodic timer with a frequency of 1Hz
 
 #hardware interrupt(rising edge on a pin) to end the timer Timer.deinit(tim)
 #hardware interrupt to take you back to the beginning of a while loop, reset elapsed, and start again
